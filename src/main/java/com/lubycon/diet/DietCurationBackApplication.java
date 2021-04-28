7148a1913a49ad43d5ffd9508f5d62baaf20c2bc
boram <boram2286@gmail.com>
2021-04-29 02:38:17 +0900

    Initial Commit

---
+++ src/main/java/com/lubycon/diet/DietCurationBackApplication.java
@@ -0,0 +1,13 @@
+package com.lubycon.diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietCurationBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DietCurationBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lubycon/diet/DietCurationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubycon.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietCurationBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
