cc9abf449ae2a04b301f1815e8f4b2d5e574cc8a
boram <boram2286@gmail.com>
2021-05-21 05:42:19 +0900

    chore: project setting

--- src/main/java/com/lubycon/eatitall/Application.java
+++ src/main/java/com/lubycon/eatitall/Application.java
@@ -1,13 +1,13 @@
-package com.lubycon.diet;
+package com.lubycon.eatitall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DietCurationBackApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(DietCurationBackApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
