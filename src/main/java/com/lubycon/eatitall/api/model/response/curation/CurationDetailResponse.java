875e6bf7a48c6764c1510833b2b4b2ba11dad760
boram <boram2286@gmail.com>
2021-06-18 01:04:52 +0900

    feat: add restaurants to curation api

--- src/main/java/com/lubycon/eatitall/api/model/response/curation/CurationDetailResponse.java
+++ src/main/java/com/lubycon/eatitall/api/model/response/curation/CurationDetailResponse.java
@@ -1,5 +1,7 @@
 package com.lubycon.eatitall.api.model.response.curation;
 
+import com.lubycon.eatitall.api.model.response.restaurant.CurationRestaurantResponse;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,5 +26,8 @@ public class CurationDetailResponse {
 
   @Nullable
   private String contents;
+
+  @Nullable
+  private List<CurationRestaurantResponse> restaurants;
 
 }
--- src/main/java/com/lubycon/eatitall/domain/curation/service/CurationService.java
+++ src/main/java/com/lubycon/eatitall/domain/curation/service/CurationService.java
@@ -4,12 +4,17 @@ import static com.lubycon.eatitall.common.util.MessageUtils.MSG_CURATION_NOT_FOU
 
 import com.lubycon.eatitall.api.model.response.curation.CurationDetailResponse;
 import com.lubycon.eatitall.api.model.response.curation.CurationResponse;
+import com.lubycon.eatitall.api.model.response.restaurant.CurationRestaurantResponse;
 import com.lubycon.eatitall.common.exception.NotFoundException;
 import com.lubycon.eatitall.domain.curation.entity.Curation;
 import com.lubycon.eatitall.domain.curation.repository.CurationJpaRepository;
+import com.lubycon.eatitall.domain.restaurant.model.KakaoMap;
+import com.lubycon.eatitall.domain.restaurant.repository.CurationRestaurantQueryRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +22,7 @@ import org.springframework.stereotype.Service;
 public class CurationService {
 
   private final CurationJpaRepository curationJpaRepository;
+  private final CurationRestaurantQueryRepository curationRestaurantQueryRepository;
 
   public List<CurationResponse> retrieveAllCurations() {
     return curationJpaRepository.findAllByIsHidden(0);
@@ -25,8 +31,35 @@ public class CurationService {
   public CurationDetailResponse retrieveCurationByCurationId(Long curationId) {
     Curation curation = curationJpaRepository.findById(curationId)
         .orElseThrow(() -> new NotFoundException(MSG_CURATION_NOT_FOUND));
+
     ModelMapper modelMapper = new ModelMapper();
-    return modelMapper.map(curation, CurationDetailResponse.class);
+    modelMapper.getConfiguration().setMatchingStrategy(
+        MatchingStrategies.STRICT);
+
+    List<CurationRestaurantResponse> restaurants = curationRestaurantQueryRepository
+        .findRestaurantsByCurationId(curationId)
+        .stream()
+        .map(curationRestaurantDto -> {
+          CurationRestaurantResponse curationRestaurantResponse = modelMapper
+              .map(curationRestaurantDto, CurationRestaurantResponse.class);
+          curationRestaurantResponse.setId(curationRestaurantDto.getRestaurantId());
+          if (curationRestaurantDto.getHashtags() != null) {
+            curationRestaurantResponse.setHashtags(curationRestaurantDto.getHashtags().split(","));
+          }
+          KakaoMap kakaoMap = KakaoMap.builder()
+              .id(curationRestaurantDto.getKakaoMapId())
+              .latitude(curationRestaurantDto.getLatitude())
+              .longitude(curationRestaurantDto.getLongitude())
+              .build();
+          curationRestaurantResponse.setKakaoMap(kakaoMap);
+          return curationRestaurantResponse;
+        })
+        .collect(Collectors.toList());
+
+    CurationDetailResponse curationDetailResponse = modelMapper
+        .map(curation, CurationDetailResponse.class);
+    curationDetailResponse.setRestaurants(restaurants);
+    return curationDetailResponse;
   }
 
 }
